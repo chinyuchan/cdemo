5648f3e18f77b958dc0fc12158fad84ba14c84f5
chinyuchan <chinyuchan@163.com>
2024-07-05 14:18:40 +0800

    arg

--- 02thread/03arg/main.c
+++ 02thread/03arg/main.c
@@ -7,9 +7,21 @@
 
 #define PI 3.14159
 
+typedef struct tag_Pyth {
+    double a;
+    double b;
+    double c;
+}PYTH, *LPPYTH;
+
 void* thread_area(void* arg) {
     double r = *(double*)arg;
     *(double*)arg = PI * r * r;
+    return NULL;
+}
+
+void* thread_pyth(void* arg) {
+    LPPYTH pyth = (LPPYTH)arg;
+    pyth->c = sqrt(pyth->a * pyth->a + pyth->b * pyth->b);
     return NULL;
 }
 
@@ -21,13 +33,29 @@ int main(void) {
 
     int err = pthread_create(&tid, NULL, thread_area, &r);
     if (err) {
-        fprintf(stderr,"pthread_create: %s\n", strerror(err));
+        fprintf(stderr,"pthread_create1: %s\n", strerror(err));
         return -1;
     }
 
-    pthread_join(tid, NULL);
+    if (err = pthread_join(tid, NULL)) {
+        fprintf(stderr, "pthread_join1: %s", strerror(err));
+    }
 
     printf("s = %g\n", r);
+    PYTH pyth;
+    printf("a = ");
+    scanf("%lf", &pyth.a);
+    printf("b = ");
+    scanf("%lf", &pyth.b);
 
+    if (err = pthread_create(&tid, NULL, thread_pyth, &pyth)) {
+        fprintf(stderr,"pthread_create2: %s\n", strerror(tid));
+        return -1;
+    }
+    if (err = pthread_join(tid, NULL)) {
+        fprintf(stderr, "pthread_join2: %s\n", strerror(err));
+        return -1;
+    }
+    printf("c = %g\n", pyth.c);
     return 0;
 }
